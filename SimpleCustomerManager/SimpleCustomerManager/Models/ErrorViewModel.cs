3356eec876bfd64d2d9780212b5bfe9d36ee1116
Koval Bohdan <kovallibodya@gmail.com>
2017-12-11 22:48:16 +0200

    Innit

---
+++ SimpleCustomerManager/SimpleCustomerManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleCustomerManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
